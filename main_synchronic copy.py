a9b94ea88735b6265f21119d2b3caece414a033c
Artem Dorofeev <a.dorofeev@ukr.net>
2023-08-30 21:12:49 +0300

    add datetime and logger

--- main_synchronic copy.py
+++ main_synchronic copy.py
@@ -1,10 +1,5 @@
 from datetime import datetime
-import logging
-from multiprocessing import cpu_count
 
-logger = logging.getLogger()
-
-logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
 
 class Number():
     def __init__(self, number: int) -> None:
@@ -21,11 +16,8 @@ class Number():
 def factorize(*number):
     result_list = []
     for i in number:
-        logger.debug(f'Started: {i} - {datetime.now()}')
         factorize_num = Number(i)
         result_list.append(factorize_num.dividing_of_numbers())
-        logger.debug(f'Done: {i} - {datetime.now()}')
-
     
     return [*result_list]
 
@@ -42,7 +34,6 @@ assert b == [1, 3, 5, 15, 17, 51, 85, 255]
 assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
 assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 print(delta_time)
-
 
 
 """
